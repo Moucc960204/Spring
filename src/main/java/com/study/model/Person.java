44e795d7002ea9ca9675e4664a75129bb27c2cea
Moucc <mou_chenchen@163.com>
2021-07-05 11:18:10 +0800

    1

---
+++ src/main/java/com/study/model/Person.java
@@ -0,0 +1,46 @@
+package com.study.model;
+
+/**
+ * @program: spring-annotation
+ * @description:
+ * @author: chenchen.mou
+ * @create: 2021-07-05 11:15
+ **/
+public class Person {
+
+    private String name;
+
+    private String age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public Person() {
+    }
+
+    public Person(String name, String age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age='" + age + '\'' +
+                '}';
+    }
+}
